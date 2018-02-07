1c6fd8d9ad4968cf06e89ffdd26891973e83afcf
Gabriel Lebec <glebec@gmail.com>
2018-02-07 15:31:09 -0500

    init: react added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 console.log('hello world')
+
+const Main = () => (
+	<p>React is Live</p>
+)
+
+ReactDOM.render(
+	<Main />,
+	document.getElementById('app')
+)
