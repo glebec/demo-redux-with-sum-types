fe7bc5b5abb4ee91c521ee19577b670a3790156c
Gabriel Lebec <glebec@gmail.com>
2018-02-07 15:25:38 -0500

    init: basic webpack and babel setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+'use strict'; // eslint-disable-line semi
+
+const { resolve } = require('path')
+
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+}
